cee46b9e0c888cdea6bcaa883286f1df3dcbad15
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-04-07 10:53:24 -0400

    giving more time for csv file read

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let data = air.travels;
     let view = new View(data,document);
     // debugger;
-  },75000)    //need time for csv file read
+  },90000)    //need time for csv file read
 })
 
 
